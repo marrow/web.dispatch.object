d3e9879e47e4696a4b641fa06272fc80a52bdc1b
Alice Bevan–McGregor <alice@gothcandy.com>
2015-03-22 16:55:58 -0400

    Correction for import path.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if sys.version_info < (2, 7):
 elif sys.version_info > (3, 0) and sys.version_info < (3, 3):
 	raise SystemExit("Python 3.3 or later is required.")
 
-exec(open(os.path.join("web", "release.py")).read())
+exec(open(os.path.join("web", "dispatch", "object", "release.py")).read())
 
 
 class PyTest(TestCommand):
