ac1112f0feb4fc84fb05b40f4b7777f3224c04da
Alice Bevan–McGregor <alice@gothcandy.com>
2015-11-21 02:56:32 -0500

    Copyright bump and description simplification.

--- web/dispatch/object/release.py
+++ web/dispatch/object/release.py
@@ -11,6 +11,6 @@ version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releasele
 version = ".".join([str(i) for i in version_info[:3]]) + ((version_info.releaselevel[0] + str(version_info.serial)) if version_info.releaselevel != 'final' else '')
 
 author = namedtuple('Author', ['name', 'email'])("Alice Bevan-McGregor", 'alice@gothcandy.com')
-description = "Object dispatch; a method for WebCore to resolve URL components to Python objects using directed attribute access."
-copyright = "2009-2014, Alice Bevan-McGregor and contributors"
+description = "Object dispatch; a method to resolve path components to Python objects using directed attribute access."
+copyright = "2009-2015, Alice Bevan-McGregor and contributors"
 url = 'https://docs.webcore.io/dispatch/object'
