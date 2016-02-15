6283b7d1adf4fb40ddb01c90a28c0a0404614997
Alice Bevan–McGregor <alice@gothcandy.com>
2016-02-14 20:36:14 -0500

    Version bump.

--- web/dispatch/object/release.py
+++ web/dispatch/object/release.py
@@ -7,10 +7,10 @@ from __future__ import unicode_literals
 from collections import namedtuple
 
 
-version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(2, 1, 0, 'alpha', 1)
+version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(2, 1, 0, 'final', 0)
 version = ".".join([str(i) for i in version_info[:3]]) + ((version_info.releaselevel[0] + str(version_info.serial)) if version_info.releaselevel != 'final' else '')
 
 author = namedtuple('Author', ['name', 'email'])("Alice Bevan-McGregor", 'alice@gothcandy.com')
 description = "Object dispatch; a method to resolve path components to Python objects using directed attribute access."
-copyright = "2009-2015, Alice Bevan-McGregor and contributors"
-url = 'https://docs.webcore.io/dispatch/object'
+copyright = "2009-2016, Alice Bevan-McGregor and contributors"
+url = 'https://github.com/marrow/web.dispatch.object'
