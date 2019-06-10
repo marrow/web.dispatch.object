b13060ca7cfaafc392df9ad287880ef09e05876e
Alice Bevan-McGregor <alice@gothcandy.com>
2019-06-09 23:23:21 -0400

    Updated package metadata, bumped minimum Python version.

--- setup.py
+++ setup.py
@@ -1,30 +1,21 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
-from __future__ import print_function
-
-import os
-import sys
-import codecs
+#!/usr/bin/env python3
 
 from setuptools import setup
+from sys import argv, version_info as python_version
+from pathlib import Path
 
 
-if sys.version_info < (3, 3):
+if python_version < (3, 6):
 	raise SystemExit("Python 3.3 or later is required.")
 
-version = description = url = author = None
-
-exec(open(os.path.join("web", "dispatch", "object", "release.py")).read())
-
-
-here = os.path.abspath(os.path.dirname(__file__))
+here = Path(__file__).resolve().parent
+version = description = url = author = None  # Populated by the next line.
+exec((here / "web" / "dispatch" / "object" / "release.py").read_text('utf-8'))
 
 tests_require = [
 		'pytest',  # test collector and extensible runner
 		'pytest-cov',  # coverage reporting
 		'pytest-flakes',  # syntax validation
-		'pytest-catchlog',  # log capture
 		'pytest-isort',  # import ordering
 	]
 
@@ -34,15 +25,22 @@ setup(
 	version = version,
 	
 	description = description,
-	long_description = codecs.open(os.path.join(here, 'README.rst'), 'r', 'utf8').read(),
+	long_description = (here / 'README.rst').read_text('utf-8'),
 	url = url,
-	download_url = 'https://pypi.org/project/web.dispatch.object/',
+	download_url = 'https://pypi.org/project/web.dispatch.object/releases',
 	
 	author = author.name,
 	author_email = author.email,
 	
 	license = 'MIT',
-	keywords = ['marrow', 'dispatch', 'web.dispatch', 'object dispatch', 'endpoint discovery', 'WebCore'],
+	keywords = [
+			'marrow',
+			'dispatch',
+			'web.dispatch',
+			'object dispatch',
+			'endpoint discovery',
+			'WebCore'
+		],
 	classifiers = [
 			"Development Status :: 5 - Production/Stable",
 			"Environment :: Console",
@@ -52,29 +50,34 @@ setup(
 			"Operating System :: OS Independent",
 			"Programming Language :: Python",
 			"Programming Language :: Python :: 3",
-			"Programming Language :: Python :: 3.3",
-			"Programming Language :: Python :: 3.4",
-			"Programming Language :: Python :: 3.5",
 			"Programming Language :: Python :: 3.6",
+			"Programming Language :: Python :: 3.7",
+			"Programming Language :: Python :: 3.8",
 			"Programming Language :: Python :: Implementation :: CPython",
 			"Programming Language :: Python :: Implementation :: PyPy",
 			"Topic :: Software Development :: Libraries :: Python Modules",
 		],
 	
-	packages = ['web.dispatch.object'],
+	project_urls = {
+			"Repository": "https://github.com/marrow/web.dispatch.object/",
+			"Documentation": "https://github.com/marrow/web.dispatch.object/#readme",
+			"Issue Tracker": "https://github.com/marrow/web.dispatch.object/issues",
+			"Funding": "https://www.patreon.com/GothAlice",
+		},
+	
+	packages = ('web.dispatch.object', ),
 	include_package_data = True,
 	package_data = {'': ['README.rst', 'LICENSE.txt']},
 	zip_safe = False,
 	
-	# Dependency Declaration
+	python_requires = '~=3.6',
 	
 	setup_requires = [
 			'pytest-runner',
-		] if {'pytest', 'test', 'ptr'}.intersection(sys.argv) else [],
+		] if {'pytest', 'test', 'ptr'}.intersection(argv) else [],
 	
 	install_requires = [
 			'web.dispatch~=3.0.1',  # Core dispatch helpers.
-			'pathlib; python_version < "3.4"',  # Path manipulation utility lib; builtin in 3.4 and 3.5.
 		],
 	
 	extras_require = dict(
